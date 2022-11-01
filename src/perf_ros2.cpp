eb7700dbe95e281207b5189168f2f38bd89e6b5c
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-11-01 09:36:54 -0400

    support sim time in ros2

--- src/perf_ros2.cpp
+++ src/perf_ros2.cpp
@@ -26,7 +26,7 @@
 void usage()
 {
   std::cout << "usage:" << std::endl;
-  std::cout << "perf <ros_topic>" << std::endl;
+  std::cout << "perf [-s use_sim_time] <ros_topic>" << std::endl;
 }
 
 using event_array_codecs::Decoder;
@@ -46,7 +46,6 @@ public:
     timer_ = rclcpp::create_timer(
       this, this->get_clock(), rclcpp::Duration::from_seconds(2.0),
       [=]() { this->timerExpired(); });
-    lastTime_ = this->get_clock()->now();
   }
   // ---------- from the EventProcessor interface:
   void eventCD(uint64_t sensor_time, uint16_t, uint16_t, uint8_t p) override
@@ -91,12 +90,18 @@ public:
 
   void timerExpired()
   {
+    if (!hasValidTime_) {
+      lastTime_ = this->get_clock()->now();
+      firstTime_ = lastTime_;
+      hasValidTime_ = true;
+    }
     const auto t = this->get_clock()->now();
+    const double t_elapsed = (t - firstTime_).nanoseconds() * 1e-9;
     const double dt = (t - lastTime_).nanoseconds() * 1e-9;  // in milliseconds
-    if (numMsgs_ != 0) {
+    if (numMsgs_ != 0 && t_elapsed > 1e-3) {
       printf(
-        "msgs: %8.2f/s drp: %2zu del: %5.2fms drft: %6.4fs", numMsgs_ / dt, numSeqDropped_,
-        delay_ / (1e6 * numMsgs_), drift_ * 1e-9);
+        "%.0f msgs: %8.2f/s drp: %2zu del: %5.2fms drft: %8.4fs", t_elapsed, numMsgs_ / dt,
+        numSeqDropped_, delay_ / (1e6 * numMsgs_), drift_ * 1e-9);
       const size_t cdTot = cdEvents_[0] + cdEvents_[1];
       if (cdTot > 0) {
         printf(" ev: %8.4f M/s %%ON: %3zu", cdTot / dt * 1e-6, (cdEvents_[1] * 100) / cdTot);
@@ -125,6 +130,7 @@ public:
   size_t cdEvents_[2]{0, 0};  // contrast change detected
   size_t trEvents_[2]{0, 0};  // trigger
 
+  rclcpp::Time firstTime_;
   rclcpp::Time lastTime_;
   uint64_t lastSeq_{0};
   uint64_t numSeqDropped_{0};
@@ -133,21 +139,43 @@ public:
   uint64_t previousStamp_{0};
   int64_t delay_{0};
   int64_t drift_{0};
+  bool hasValidTime_{false};
 };
 
 int main(int argc, char ** argv)
 {
-  std::string topic;
   rclcpp::init(argc, argv);
-  switch (argc) {
-    case 2:
-      topic = argv[1];
-      break;
-    default:
-      usage();
-      exit(-1);
+  int opt;
+  std::string topic;
+  bool useSimTime(false);
+  while ((opt = getopt(argc, argv, "sh")) != -1) {
+    switch (opt) {
+      case 's':
+        useSimTime = true;
+        break;
+      case 'h':
+        usage();
+        return (-1);
+        break;
+      default:
+        std::cout << "unknown option: " << opt << std::endl;
+        usage();
+        return (-1);
+        break;
+    }
   }
-  auto node = std::make_shared<Perf>(topic, rclcpp::NodeOptions());
+  if (optind != argc - 1) {
+    std::cout << "expected topic!" << std::endl;
+    usage();
+    return (-1);
+  }
+  topic = argv[optind];
+  auto nopt = useSimTime ? rclcpp::NodeOptions().append_parameter_override("use_sim_time", true)
+                         : rclcpp::NodeOptions();
+  if (useSimTime) {
+    std::cout << "USING SIMULATION TIME!" << std::endl;
+  }
+  auto node = std::make_shared<Perf>(topic, nopt);
   rclcpp::spin(node);  // should not return
   rclcpp::shutdown();
   return 0;
